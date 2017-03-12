6a5d22bf66051ff42d7d39227bdb732a1c03d960
Jay Ravaliya <jayrav13@gmail.com>
2017-03-12 03:00:32 -0400

    Allowed for command line arguments for customization

--- tickers.py
+++ tickers.py
@@ -6,6 +6,19 @@ import requests
 import json
 from datetime import date, timedelta
 
+
+year = None
+if len(sys.argv) > 1:
+	year = sys.argv[1]
+else:
+	year = 1980
+
+filename = None
+if len(sys.argv) > 2:
+	filename = sys.argv[2]
+else:
+	filename = 'result.json'
+
 # Return data from CSV.
 def get_csv(url, delimiter=","):
 	response = requests.get(url)
@@ -14,7 +27,7 @@ def get_csv(url, delimiter=","):
 
 # Generate URL for ticker-specific historical data.
 def generate_url(ticker):
-	return "http://chart.finance.yahoo.com/table.csv?s=%s&a=0&b=0&c=1980&g=d&ignore=.csv" % ticker
+	return "http://chart.finance.yahoo.com/table.csv?s=%s&a=0&b=1&c=%s&g=d&ignore=.csv" % (ticker, year)
 
 # Pull all tickers in CSV
 tickers = get_csv('http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download')
@@ -34,8 +47,8 @@ for ticker in tickers:
 	data.append(ticker)
 	print ticker["Symbol"] + " - " + ticker["Name"]
 
-f = open('result.json', 'w')
-f.write(json.dumps(data))
+f = open(filename, 'w')
+f.write(json.dumps(data, indent=4, sort_keys=True))
 f.close()
 
 
